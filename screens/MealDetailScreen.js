7558345aa1fcac4fb1cff3ee941070b6d27b4f0d
Natan <natanlaraferreira@gmail.com>
2020-05-09 21:49:43 -0300

    Switching the favorites icon and Fallback Text

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -19,6 +19,11 @@ const MealDetailScreen = props => {
 
   const availableMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam('mealId');
+
+  const currentMealIsFavorite = useSelector(state =>
+    state.meals.favoriteMeals.some(meal => meal.id === mealId)
+  );
+
   const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
   const dispatch = useDispatch();
@@ -31,6 +36,10 @@ const MealDetailScreen = props => {
     // props.navigation.setParams({ mealTitle: selectedMeal.title });
     props.navigation.setParams({toggleFav: toggleFavoriteHandler});
   }, [toggleFavoriteHandler]);
+
+  useEffect(() => {
+    props.navigation.setParams({ isFav: currentMealIsFavorite });
+  }, [currentMealIsFavorite]);
 
   return (
     <ScrollView>
@@ -56,6 +65,7 @@ MealDetailScreen.navigationOptions = navigationData => {
   //const mealId = navigationData.navigation.getParam('mealId');
   const mealTitle = navigationData.navigation.getParam('mealTitle');
   const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+  const isFavorite = navigationData.navigation.getParam('isFav');
   //const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
     headerTitle: mealTitle,
@@ -63,7 +73,7 @@ MealDetailScreen.navigationOptions = navigationData => {
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Favorite"
-          iconName="ios-star"
+          iconName={isFavorite ? 'ios-star' : 'ios-star-outline'}
           onPress={toggleFavorite} //toggleFavorite aponta para a função toggleFavoriteHandler
         />
       </HeaderButtons>
