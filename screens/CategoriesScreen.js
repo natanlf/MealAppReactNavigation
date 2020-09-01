7bb6b90933b9f906e2a511de3daf5c9b3ce84f28
Natan <natanlaraferreira@gmail.com>
2020-08-31 22:26:19 -0300

    Style, refactoring and adding data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { View, Text, StyleSheet, Button, TouchableOpacity, FlatList, Platform } from 'react-native';
 import { CATEGORIES  } from '../data/dummy-data';
 import Colors from '../constants/Colors';
+import CategoryGridTitle from '../components/CategoryGridTitle';
 
 const CategoriesScreen = props => {
 
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity style={styles.gridItem}
-      onPress={() => {
-        props.navigation.navigate({ 
-          routeName: 'CategoryMeals', 
-          params: {
-            categoryId: itemData.item.id
-          }
-        })
-      }}>
+      <CategoryGridTitle style={styles.gridItem}
+        title={itemData.item.title} //funciona como o @Input enviando dados ao componente filho
+        color={itemData.item.color}
+        onSelect={() => { //evento que é recebido ao clicar no componente CategoryGridTitle. Esse é o nome do evento = onSelect (não sei se é padrão ou se pode ser qualquer nome)
+          props.navigation.navigate({
+            routeName: 'CategoryMeals', 
+            params: {
+              categoryId: itemData.item.id
+            }
+          })
+        }}
+      >
         <Text>{itemData.item.title}</Text>
-      </TouchableOpacity>
+      </CategoryGridTitle>
     )
   }
 
