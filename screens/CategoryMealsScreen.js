047c4af4c3d7056012cedeb0df8fdeb8db218160
Natan <natanlaraferreira@gmail.com>
2020-09-04 22:18:42 -0300

    Meals by Categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Platform } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
+import { View, Text, StyleSheet, Button, Platform, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
 
-  const catId = props.navigation.getParam('categoryId');
-  const selectedCategory = CATEGORIES.find(e => e.id === catId)
+  const renderMealItem = itemData => {
+    return(
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={ () => {
+          props.navigation.navigate({
+            routeName: 'Meal Detail',
+            params: {
+              mealId: itemData.item.id
+            }
+          })
+        }}
+      />
+    )
+  }
 
+  const catId = props.navigation.getParam('categoryId');
+  
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+  
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button title="Go to Detail Screen" onPress={() => {
-        props.navigation.navigate({
-          routeName: 'MealDetail'
-        })
-      }}/>
-      <Button title="Go Back" onPress={() => {
-        props.navigation.pop(); //volta a tela anterior
-      }}/>
-
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={ (item, index) => item.id }
+        renderItem={renderMealItem}
+        style={{width: '100%'}}
+      />
     </View>
   );
 };
@@ -38,7 +56,8 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 15
   }
 });
 
