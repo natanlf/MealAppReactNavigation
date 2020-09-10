ec4d156a4949c565e7813eac68185f1bedb0dc93
Natan <natanlaraferreira@gmail.com>
2020-09-09 22:00:42 -0300

    Adding MealList Component (refactoring)

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, Platform, FlatList } from 'react-native';
 import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import MealList from '../components/MealList';
 
 const CategoryMealScreen = props => {
-
-  const renderMealItem = itemData => {
-    return(
-      <MealItem
-        title={itemData.item.title}
-        image={itemData.item.imageUrl}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        onSelectMeal={ () => {
-          props.navigation.navigate({
-            routeName: 'MealDetail',
-            params: {
-              mealId: itemData.item.id
-            }
-          })
-        }}
-      />
-    )
-  }
 
   const catId = props.navigation.getParam('categoryId');
   
@@ -31,15 +11,9 @@ const CategoryMealScreen = props => {
     meal => meal.categoryIds.indexOf(catId) >= 0
   );
   
+  /* Preciso passar o navigation para conseguir navegar quando houver um clique em um item da lista */
   return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={ (item, index) => item.id }
-        renderItem={renderMealItem}
-        style={{width: '100%'}}
-      />
-    </View>
+    <MealList listData={displayedMeals} navigation={props.navigation} />
   );
 };
 
@@ -51,14 +25,5 @@ CategoryMealScreen.navigationOptions = navigationData => {
     headerTitle: selectedCategory.title
   }
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 15
-  }
-});
 
 export default CategoryMealScreen;
